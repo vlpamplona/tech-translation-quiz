f8f410d2fbc799081d7d5450bbf3c93c8dfa4662
Vinícius Lucatelli Pamplona <viniciuslpamplona@gmail.com>
2026-03-16 23:00:57 -0300

    Alteração e melhorias na lógica

--- main.py
+++ main.py
@@ -1,29 +1,39 @@
 import random
 import re
+import sys
 import google.generativeai as genai
 import json
 
-# Sua Chave API criada no google AI
+# Sua Chave
 GOOGLE_API_KEY = "COLE_AQUI_SUA_API_KEY"
 genai.configure(api_key=GOOGLE_API_KEY)
 
-def obtem_lista_perguntas():
-    
+ # guarda os termos gerados para evitar trazer termos repetidos
+historico_termos = []
+score = 0
+
+
+def obtem_termos():
+
     # Nota: Caso a versão 2.5 não esteja disponível na sua região/conta, 
     # altere para 'gemini-2.0-flash' ou 'gemini-1.5-flash'
     # Usando o modelo disponível
     model = genai.GenerativeModel('gemini-2.5-flash')
+
     
-    # para tentar evitar que os termos venham repetidos a cada acesso à IA
+    # lista para tentar evitar que os termos venham repetidos a cada acesso à IA
     # por ser um codigo apenas para estudo e aprendizado, essa solucao é funcional
     categorias = ["DevOps", "Banco de Dados", "Frontend", "Backend","Cibersegurança", 
-                  "Cloud Computing", "Inteligência Artificial", "Hardware"]
+                  "Computação em Nuvem", "Inteligência Artificial", "Hardware"]
     categoria_da_vez = random.choice(categorias)
     
-    # altere a quantidade que desejar, dos termos que serao retornados a cada acesso
+    
+    # a ideia aqui é trazer uma qtd de termos por acesso, para que o usuário possa responder o quiz
+    # sem fazer um novo acesso, evitando exceder a quota na api
+    # altere a quantidade que desejar dos termos que serao retornados a cada acesso
     prompt = f"""
-    Gere uma lista com 10 termos técnicos aleatórios, focando na área de {categoria_da_vez}
-    Garanta que não foram citados anteriormente.
+    Gere uma lista com EXATAMENTE 3 termos técnicos aleatórios, focando na área de {categoria_da_vez}
+    Exclua os seguintes termos {historico_termos}.
     Retorne APENAS um JSON no seguinte formato:
     [
     {{"palavra": "Termo em Português", "traducao": "Termo em Inglês"}}
@@ -33,6 +43,7 @@ def obtem_lista_perguntas():
     
     try:
         response = model.generate_content(prompt)
+        
         # Garante que pegamos apenas o JSON, mesmo que a IA retorne lixo
         # Extrai apenas o conteúdo entre colchetes [ ]
         json_match = re.search(r'\[.*\]', response.text, re.DOTALL)
@@ -46,39 +57,65 @@ def obtem_lista_perguntas():
             print(f"Erro na conexão: {e}")
         return None
 
-def quiz():
+def iniciar_quiz():
+    global historico_termos
+    global score
+
     print("\n--- 🤖 TECH TRANSLATION QUIZ ---\n")
-    print("Sorteando palavra...")
+    print("Buscando os termos...")
     
-    perguntas = obtem_lista_perguntas()
+    termos = obtem_termos()
         
-    if not perguntas:
+    if not termos:
+        print("\n❌ Falha ao obter os termos\n")
         return
+        
+    total = 0
     
-    score = 0
-    total = len(perguntas)
-    
-    for item in perguntas:
+    for item in termos:
+            
         # O que aparece para o usuário (em Portugues)
         palavra = item.get('palavra')
         # A resposta correta (em Ingles)
         traducao = item.get('traducao')
         
+         # Só adiciona se NÃO existe
+        if palavra not in historico_termos:
+            historico_termos.append(palavra)
+        
+        total = len(historico_termos)
+
         print(f"\nComo se diz '{palavra}' em inglês?")
         answer = input("Resposta: ").strip().lower()
         
         if answer == traducao.lower():
-            print("✨ EXATO! Você está afiado.")
+            print("✨ EXATO! Você respondeu corretamente.")
             score += 1
         else:
             print(f"❌ Errado! O termo correto é: {traducao}")
     
+    main_quiz(score, total)
     
-    print("\n" + "="*30)
-    print(f"RESUMO DO QUIZ")
-    print(f"Acertos: {score} de {total}")
-    print(f"Desempenho: {(score/total)*100:.2f}%")
-    print("="*30)
+
+def main_quiz(score, total):
+    while True:
+        encerrar = input("\nDigite C para continuar ou S para sair, e tecle ENTER: ").strip().upper()   
+        if encerrar == "C":
+            iniciar_quiz()  # continua o quiz
+        elif encerrar == "S":
+            # imprime o resumo
+            print("\n" + "="*30)
+            print(f"RESUMO DO QUIZ")
+            print(f"Acertos: {score} de {total}")
+            if total > 0:
+                print(f"Desempenho: {(score/total)*100:.2f}%")
+            else:
+                print("Desempenho: N/A (nenhum termo respondido)")
+            print("="*30)
+            sys.exit()  # Sair do programa
+        else:
+            print("❌ Digite apenas C ou S")
     
+        
 if __name__ == "__main__":
-    quiz()
+    iniciar_quiz()
